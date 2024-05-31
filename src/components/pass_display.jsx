647f0d35cadf51a53e85244289d255840ff230eb
Isaac Reyes <isaac.reyalves@gmail.com>
2024-05-31 19:04:06 -0300

    feat: recreated basic app structure

---
+++ src/components/pass_display.jsx
@@ -0,0 +1,10 @@
+export default function PassDisplay() {
+    return (
+        <div>
+            <span>PTx1f5DaFX</span>
+            <span className="material-symbols-rounded">
+                file_copy
+            </span>
+        </div>
+    )
+}
